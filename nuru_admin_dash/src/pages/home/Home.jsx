43e4cfd3a56a4950a0bafd6884e1cffae662648c
wangare <marywangareee@gmail.com>
2022-10-20 21:35:53 +0300

    implemented sidebar

--- nuru_admin_dash/src/pages/home/Home.jsx
+++ nuru_admin_dash/src/pages/home/Home.jsx
@@ -1,3 +1,4 @@
+import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
 import Widget from "../../components/widget/Widget";
@@ -8,6 +9,7 @@ import Table from "../../components/table/Table";
 const Home = () => {
   return (
     <div className="home">
+      <Sidebar />
       <div className="homeContainer">
         <Navbar />
         <div className="total-sms">
--- nuru_dashboard/README.md
+++
@@ -1,1 +0,0 @@
-# Nuru_frontend
